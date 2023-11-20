2f16820a063895512842dc782234029a1ee132cb
Kacper Kaluza <97208353+kacperkaluza@users.noreply.github.com>
2023-11-20 11:04:03 +0100

    Create Buttons.jsx

---
+++ src/components/Buttons.jsx
@@ -0,0 +1,13 @@
+import Deal from "./Buttons/Deal";
+import Fold from "./Buttons/Fold";
+import Start from "./Buttons/Start";
+
+export default function Buttons() {
+  return (
+    <>
+      <Start />
+      <Deal />
+      <Fold />
+    </>
+  );
+}
