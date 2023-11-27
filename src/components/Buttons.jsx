d1adfdedb665d0f1d03bfcf0d1ac1acbbcf7d922
Kacper Kaluza <97208353+kacperkaluza@users.noreply.github.com>
2023-11-27 08:57:23 +0100

    Update Buttons.jsx

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -1,8 +1,10 @@
+import { useGameStatus } from "../hooks/useGameStatus";
 import Deal from "./Buttons/Deal";
 import Fold from "./Buttons/Fold";
 import Start from "./Buttons/Start";
 
 export default function Buttons() {
+  const [gameStatus, changeGameStatus] = useGameStatus();
   return (
     <>
       {gameStatus != "onGoing" && <Start />}
